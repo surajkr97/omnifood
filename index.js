ff4dfb9e527fa65a7b656472c1ffcd7100b150d3
Suraj Kumar <surajkumar06174@gmail.com>
2023-05-04 02:17:29 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 /* FORM FUNCTIONALITY */
-// function submission(event) {
-// 	event.preventDefault();
-// 	let name = document.querySelector("#user-name").value;
-// 	let planChoice = document.querySelector("#user-plan").value;
-// 	let message = document.querySelector(".submission");
-// 	if (planChoice !== "") {
-// 		message.style.display = "block";
-// 		message.textContent = `Thank you for your interest in the ${planChoice} plan, ${name}. Unfortunately, Omnifood is a fictional company so there's no free meal to send you.`;
-// 	} else {
-// 		message.style.display = "block";
-// 		message.textContent = `Sorry, you need to choose a plan to proceed!`;
-// 	}
-// }
+function submission1(event) {
+	event.preventDefault();
 
-let sbtn = document.getElementById('sbtn');
-sbtn.addEventListener("click", myfunc1)
+	let name = document.querySelector("#user-name").value;
+	let planChoice = document.querySelector("#user-plan").value;
 
-let name = document.querySelector("#user-name").value;
-let planChoice = document.querySelector("#user-plan").value;
+	let message = document.querySelector(".submission");
 
-let message = document.querySelector(".submission");
-
-function myfunc1() {
 	if (planChoice !== "") {
 		message.style.display = "block";
 		message.textContent = `Thank you for your interest in the ${planChoice} plan, ${name}. Unfortunately, Omnifood is a fictional company so there's no free meal to send you.`;
@@ -32,4 +17,5 @@ function myfunc1() {
 }
 
 const subscribeForm = document.querySelector("#form-subscribe-element");
-subscribeForm.addEventListener("submit", submission);
+subscribeForm.addEventListener("click", submission1);
+
