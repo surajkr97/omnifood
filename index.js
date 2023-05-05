082c4d0e88d1bbb156b020fe8a4a074e90718801
Suraj Kumar <surajkumar06174@gmail.com>
2023-05-06 00:00:52 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-/* FORM FUNCTIONALITY */
+/* FORM FUNCTIONALITY INCLUDING VALIDATION*/
 function submission1(event) {
 	event.preventDefault();
 
 	let name = document.querySelector("#user-name").value;
 	let planChoice = document.querySelector("#user-plan").value;
 
-	console.log(name)
-
 	let message = document.querySelector(".submission");
 
 	let rg = /[a-zA-Z0-9_\-\.]+[@][a-zA-Z]+[\.][a-zA-Z]{2,3}$/;
 	let em = document.getElementById("user-email");
 
-	// if (planChoice == "") {
-	// 	message.style.display = "block";
-	// 	message.textContent = "Sorry, you need to choose a plan to proceed!";
-	// }
 
-	if (em.value == "") {
+	if (name == "") {
+		message.style.display = "block";
+		message.textContent = "Sorry, you need to put your name to proceed!";
+	}
+	
+	else if (em.value == "") {
 		message.style.display = "block";
 		message.textContent = "Sorry, you need to put your email id to proceed!";
 	}
@@ -27,9 +26,14 @@ function submission1(event) {
 		message.textContent = "Please, put your valid email id to proceed!";
 	}
 
+	else if (planChoice == "") {
+		message.style.display = "block";
+		message.textContent = "Sorry, you need to select your plan to proceed!";
+	}
+
 	else {
 		message.style.display = "block";
-		message.textContent = "Thank you for your interest in the ${planChoice} plan, ${name}. Unfortunately, Omnifood is a fictional company so there's no free meal to send you.";
+		message.textContent = "Thank you for your interest in the "+planChoice+" plan, "+name+". Unfortunately, Omnifood is a fictional company so there's no free meal to send you.";
 	}
 }
 
