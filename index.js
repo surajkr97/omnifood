6ee63d4e849913392e72f5aa9bc136850070a5b8
Suraj Kumar <surajkumar06174@gmail.com>
2023-05-05 22:40:08 +0530

    modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -9,13 +9,22 @@ function submission1(event) {
 
 	if (planChoice !== "") {
 		message.style.display = "block";
-		message.textContent = `Thank you for your interest in the ${planChoice} plan, ${name}. Unfortunately, Omnifood is a fictional company so there's no free meal to send you.`;
+		message.textContent = "Thank you for your interest in the ${planChoice} plan, ${name}. Unfortunately, Omnifood is a fictional company so there's no free meal to send you.";
 	} else {
 		message.style.display = "block";
-		message.textContent = `Sorry, you need to choose a plan to proceed!`;
+		message.textContent = "Sorry, you need to choose a plan to proceed!";
 	}
 }
 
 const subscribeForm = document.querySelector("#form-subscribe-element");
 subscribeForm.addEventListener("click", submission1);
 
+/* EMAIL VERIFICATION */
+let rg = /[a-zA-Z0-9_\-\.]+[@][a-zA-Z]+[\.][a-zA-Z]{2,3}$/;
+let bg = /[a-zA-Z0-9]+[@][a-zA-Z]+[\.][a-zA-Z]{2,3}$/;
+let em = document.getElementById('user-email')
+
+if(em.value == ""){
+	message.style.display = "block";
+	message.textContent = "Sorry, you need to put your email id to proceed!";
+}
