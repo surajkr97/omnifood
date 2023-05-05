3a196ae210652e03338e5a40b0d40d48489fc41c
Suraj Kumar <surajkumar06174@gmail.com>
2023-05-05 23:44:58 +0530

    modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -5,26 +5,33 @@ function submission1(event) {
 	let name = document.querySelector("#user-name").value;
 	let planChoice = document.querySelector("#user-plan").value;
 
+	console.log(name)
+
 	let message = document.querySelector(".submission");
 
-	if (planChoice !== "") {
+	let rg = /[a-zA-Z0-9_\-\.]+[@][a-zA-Z]+[\.][a-zA-Z]{2,3}$/;
+	let em = document.getElementById("user-email");
+
+	// if (planChoice == "") {
+	// 	message.style.display = "block";
+	// 	message.textContent = "Sorry, you need to choose a plan to proceed!";
+	// }
+
+	if (em.value == "") {
+		message.style.display = "block";
+		message.textContent = "Sorry, you need to put your email id to proceed!";
+	}
+
+	else if (!(rg.test(em.value))) {
+		message.style.display = "block";
+		message.textContent = "Please, put your valid email id to proceed!";
+	}
+
+	else {
 		message.style.display = "block";
 		message.textContent = "Thank you for your interest in the ${planChoice} plan, ${name}. Unfortunately, Omnifood is a fictional company so there's no free meal to send you.";
-	} else {
-		message.style.display = "block";
-		message.textContent = "Sorry, you need to choose a plan to proceed!";
 	}
 }
 
 const subscribeForm = document.querySelector("#form-subscribe-element");
 subscribeForm.addEventListener("click", submission1);
-
-/* EMAIL VERIFICATION */
-let rg = /[a-zA-Z0-9_\-\.]+[@][a-zA-Z]+[\.][a-zA-Z]{2,3}$/;
-let bg = /[a-zA-Z0-9]+[@][a-zA-Z]+[\.][a-zA-Z]{2,3}$/;
-let em = document.getElementById('user-email')
-
-if(em.value == ""){
-	message.style.display = "block";
-	message.textContent = "Sorry, you need to put your email id to proceed!";
-}
