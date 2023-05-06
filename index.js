3f8d26198f1beb01f335b75842c57c6a4bceb659
Suraj Kumar <surajkumar06174@gmail.com>
2023-05-07 02:52:35 +0530

    modified:   index.html 	modified:   index.js 	modified:   queries.css 	modified:   styles.css

--- index.js
+++ index.js
@@ -54,3 +54,15 @@ const navObserver = new IntersectionObserver(
 	})
 
 navObserver.observe(onSecondPage);
+
+
+/* HOVER FUNCTIONALITY FOR MOBILE */
+document.addEventListener("touchstart", function () {}, true);
+
+/* HAMBURGER MENU */
+const navButton = document.querySelector(".mobile-nav-btn");
+const headerElement = document.querySelector(".header");
+
+navButton.addEventListener("click", function () {
+	headerElement.classList.toggle("nav-open");
+});
